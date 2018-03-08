3be22e45215ddb1374b51dfc0798c20520736d9f
ChouaibHamek <11193161+ChouaibHamek@users.noreply.github.com>
2018-03-08 12:48:33 +0200

    Create containers folder

---
+++ src/containers/posts/posts_reducer.js
@@ -0,0 +1,12 @@
+import { FETCH_POSTS } from './posts_actions'
+import _ from 'lodash'
+
+export default function (state={}, action ) {
+  switch(action.type){
+  case FETCH_POSTS:
+    console.log(action.payload.data)
+    return _.mapKeys(action.payload.data, "id")
+  default:
+    return state
+  }
+}
