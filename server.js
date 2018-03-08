2d72cb43804e9c40ad8f0eacc1f6bc6d9c8e2412
ChouaibHamek <11193161+ChouaibHamek@users.noreply.github.com>
2018-03-08 13:58:51 +0200

    Fix app path in server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist/index.html'))
