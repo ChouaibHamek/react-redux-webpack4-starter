122ee76342e33866471d5b5d020587d3b4e6b9ec
ChouaibHamek <11193161+ChouaibHamek@users.noreply.github.com>
2018-03-08 19:22:02 +0200

    Add dev Server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../app/dist')));
+var development = process.env.NODE_ENV !== 'production';
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../app/dist/index.html'))
-});
+if (development){
 
-app.listen(port);
-console.log('Server started..');
+  app.get('/api/hello', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.send({ express: 'Hello From Express' });
+  });
+
+  console.log('Development express server running..');
+
+} else {
+  app.use(express.static(path.join(__dirname, '../app/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../app/dist/index.html'))
+  });
+
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
