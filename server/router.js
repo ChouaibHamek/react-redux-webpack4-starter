29aa39658e7171fda401be94a9592378c389c362
ChouaibHamek <11193161+ChouaibHamek@users.noreply.github.com>
2018-03-17 13:37:31 +0200

    Add API CORS

--- server/router.js
+++ server/router.js
@@ -4,9 +4,12 @@ const passportService = require('./services/passport')
 const requireAuth = passportService.authenticate('jwt', { session: false })
 const requireSignin = passportService.authenticate('local', { session: false })
 
+const allowedOrigin = ['*']
+
 module.exports = (app) => {
   app.get('/api/hello', (req, res) => {
-    res.json({ express: 'Hello From Express' })
+    res.header('Access-Control-Allow-Origin', ...allowedOrigin)
+    res.send({ express: 'Hello From Express' })
   })
   app.post('/signin', requireSignin, Authentication.signin)
   app.post('/signup', Authentication.signup)
