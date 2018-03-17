0382386bcc537442f88963942648bdef09882e65
ChouaibHamek <11193161+ChouaibHamek@users.noreply.github.com>
2018-03-17 13:39:31 +0200

    Fix port in home page actions

--- app/src/containers/homePage/actions.js
+++ app/src/containers/homePage/actions.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const GET_EXPRESS_DATA = 'GET_EXPRESS_DATA'
 
-const URL = 'http://localhost:5000/api/hello'
+const URL = 'http://localhost:3090/api/hello'
 
 export function fetchExpressData() {
   const request = axios.get(URL)
