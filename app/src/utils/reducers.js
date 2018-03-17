84b989f54c9cebe747beb634f6306859630d4aca
ChouaibHamek <11193161+ChouaibHamek@users.noreply.github.com>
2018-03-17 17:31:03 +0200

    Remove posts container

--- app/src/utils/reducers.js
+++ app/src/utils/reducers.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux'
-import postsReducer from '../containers/posts/reducer'
 import expressReducer from '../containers/homePage/reducer'
 
 export default combineReducers({
   express: expressReducer,
-  posts: postsReducer,
 })
