4dd76aaf324727792817454295259792fb607ead
ChouaibHamekC <chouaib.hamek@cleac.fi>
2018-03-21 15:17:09 +0200

    Add requireAuth conditional render

--- app/src/containers/app/require_auth.jsx
+++ app/src/containers/app/require_auth.jsx
@@ -5,18 +5,24 @@ export default function(ComposedComponent) {
   class Authentication extends Component {
     componentWillMount() {
       if (!this.props.authenticated) {
+        console.log('componentWillMount')
         this.props.history.push('/');
       }
     }
 
     componentWillUpdate(nextProps) {
       if (!nextProps.authenticated) {
+        console.log('componentWillUpdate')
         this.props.history.push('/');
       }
     }
 
+    renderComposedComponent() {
+      return this.props.authenticated? <ComposedComponent {...this.props} />: null
+    }
+
     render() {
-      return <ComposedComponent {...this.props} />
+      return this.renderComposedComponent()
     }
   }
 
